e77946f81cee2eccd8dbf38bc4867b1605da5084
Jürgen Gerhardt <juribolka@hotmail.de>
2021-11-17 15:52:59 +0100

    Update

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -5,85 +5,117 @@
         // Als Benutzer möchte ich zwei Zahlen eingeben,
         // um deren Summe berechnen zu lassen. 
         // Eingabe und Ausgabe
-        string zahl1 = HoleBenutzereingabe("Bitte gib die erste Zahl ein: ");
-        Console.WriteLine("********************************");
-        string zahl2 = HoleBenutzereingabe("Bitte gib die zweite Zahl ein: ");
-        Console.WriteLine("********************************");
-        string operation = HoleBenutzereingabe("Bitte gib die auszuführende Operation ein (+,*,/ oder -): ");
+        double ersteZahl = 0;
+        double zweiteZahl = 0;
+
+        bool eingabeErfolgreich = false;
+        while (!eingabeErfolgreich)
+        {
+            string zahl1 = HoleBenutzerEingabe("Bitte gib die erste Zahl ein: ");
+            Console.WriteLine("********************************");
+            eingabeErfolgreich = double.TryParse(zahl1, out ersteZahl);
+            if (!eingabeErfolgreich)
+            {
+                Console.WriteLine("Bist du zu doof eine Zahl einzugeben?");
+            }
+        }
+
+        bool eingabeErfolgreich1 = false;
+        while (!eingabeErfolgreich1)
+        {
+            string zahl2 = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein: ");
+            Console.WriteLine("********************************");
+            eingabeErfolgreich1 = double.TryParse(zahl2, out zweiteZahl);
+            if (!eingabeErfolgreich1)
+            {
+                Console.WriteLine("Immernoch es muss eine Zahl eingegeben werden!");
+            }
+        }
+
+
+        string operation = HoleBenutzerEingabe("Bitte gib die auszuführende Operation ein (+,*,/ oder -): ");
         Console.WriteLine("*******************************************************");
 
         // Konvertierung von Text in Ganzzahlen und GLeitkommazahlen
-        // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist.
-        double ersteZahl = Convert.ToDouble(zahl1);
-        double zweiteZahl = Convert.ToDouble(zahl2);
+        // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist.   
 
         // Berechnung ausführen
-        double resultat = 0;
-        switch (operation)
+        var resultat = Berechnung(ersteZahl, zweiteZahl, operation);
+
+        // Ausgabe
+        HoleBenutzerEingabe("Zum beenden bitte Return drücken.");
+
+        // Berechnung als eigene Methode 
+        double Berechnung(double ersteZahl, double zweiteZahl, string operation)
         {
-            case "+":
-                resultat = Addiere(ersteZahl, zweiteZahl);
-                Console.WriteLine("Die Summe ist: {0}", resultat);
-                break;
-            case "-":
-                resultat = Subtrahiere(ersteZahl, zweiteZahl);
-                Console.WriteLine("Die Differenz ist: {0}", resultat);
-                break;
-            case "*":
-                resultat = Multipliziere(ersteZahl, zweiteZahl);
-                Console.WriteLine("Der Produktfaktor ist: {0}", resultat);
-                break;
-            case "/":
-                resultat = quotientenwert(ersteZahl, zweiteZahl);
-                Console.WriteLine("Der Quotientwert ist: {0}", resultat);
-                break;
-            default:
-                Console.WriteLine("Keine Gültige Operation!!!");
-                break;
+            resultat = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    resultat = Addiere(ersteZahl, zweiteZahl);
+                    Console.WriteLine("Die Summe ist: {0}", resultat);
+                    break;
+                case "-":
+                    resultat = Subtrahiere(ersteZahl, zweiteZahl);
+                    Console.WriteLine("Die Differenz ist: {0}", resultat);
+                    break;
+                case "*":
+                    resultat = Multipliziere(ersteZahl, zweiteZahl);
+                    Console.WriteLine("Das Produkt ist: {0}", resultat);
+                    break;
+                case "/":
+                    resultat = quotientenwert(ersteZahl, zweiteZahl);
+                    Console.WriteLine("Der Quotientwert ist: {0}", resultat);
+                    break;
+                default:
+                    Console.WriteLine("Keine Gültiger Operator!!!");
+                    break;
+            }
+            return resultat;
         }
-        HoleBenutzereingabe("Zum beenden bitte Return drücken.");
-    }
 
         // Methode zur Ausgabe des eingegebenen Strings als Zahl
-    static string HoleBenutzereingabe(string ausgabeText)
-    {
-        Console.Write(ausgabeText);
-        string zahl1 = Console.ReadLine();
+        static string HoleBenutzerEingabe(string ausgabeText)
+        {
+            Console.Write(ausgabeText);
+            string zahl1 = Console.ReadLine();
 
-        return zahl1; 
+            return zahl1;
+        }
+
+        // Methode(Addition)
+        static double Addiere(double ersterSummand, double zweiterSummand)
+        {
+            double summe = ersterSummand + zweiterSummand;
+
+            return summe;
+
+        }
+
+        // Methode(Subtraktion)
+        static double Subtrahiere(double minuend, double subtrahent)
+        {
+            var differenz = minuend - subtrahent;
+
+            return differenz;
+        }
+
+        // Methode(Multiplikation)
+        static double Multipliziere(double produkt, double produkt2)
+        {
+            double produktfaktor = produkt * produkt2;
+
+            return produktfaktor;
+        }
+
+        // Methode(Division)
+        static double quotientenwert(double divident, double divisor)
+        {
+            double quotientenwert = divident / divisor;
+
+            return quotientenwert;
+        }
     }
-
-        // Berechnungen als eigene Methode(Addition)
-    static double Addiere(double ersterSummand, double zweiterSummand)
-    {
-        double summe = ersterSummand + zweiterSummand;
-        
-        return summe;
-
-    }
-
-        // Berechnung als eigene Methode(Subtraktion)
-    static double Subtrahiere(double minuend, double subtrahent)
-    {
-        double differenz =  minuend - subtrahent;
-        
-        return differenz;
-    }
-
-        // Berechnung als eigene Methode(Multiplikation)
-    static double Multipliziere(double produkt, double produkt2)
-    {
-        double produktfaktor = produkt * produkt2;
-
-        return produktfaktor;
-    }
-
-    // Berechnung als eigene Methode(Division)
-    static double quotientenwert(double divident, double divisor)
-    {
-        double quotientenwert = divident / divisor;
-
-        return quotientenwert;
-    }
- }
+}
 
